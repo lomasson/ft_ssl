3da091fe0ecb9192492a37fe8f8e45ab4ab7d292
snaker <loic.masson67@hotmail.fr>
2024-07-03 09:40:49 +0100

    fix: sha256 big file + delete swap bit on print

--- srcs/sha256/sha256.c
+++ srcs/sha256/sha256.c
@@ -55,11 +55,6 @@ static	void convert_big_endian_msg(const t_sha_msg vars, t_sha_msg* res){
 	}
 }
 
-static	void convert_big_endian_hash(const t_sha_buf vars, u_int32_t* res){
-	for(unsigned int i = 0; i < 8; i++){
-		res[i] = __builtin_bswap32(vars.h[i]);
-	}
-}
 void printBits(unsigned int num){
 	unsigned int size = sizeof(unsigned int);
 	unsigned int maxPow = 1<<(size*8-1);
@@ -81,53 +76,21 @@ static	void hash(const int fd, t_sha_buf *mdbuffer)
 	while (42)
 	{
 		memset(&buffer, 0, SHA256_BLOCK_LEN);
-		// u_int32_t read_buffer = 0;
-		// u_int8_t	mybuffer[64];
-		// for(int i = 0; i < SHA256_BLOCK_LEN;i++)
-		// {
-		// 	read(fd, &(mybuffer[i]), 1);
-		// }
-		// printf("addr[0]: %p : %x\n", &mybuffer[0], mybuffer[0]);
-		// printf("addr[1]: %p : %x\n", &mybuffer[1], mybuffer[1]);
-		// printf("addr[2]: %p : %x\n", &mybuffer[2], mybuffer[2]);
-		// printf("addr[3]: %p : %x\n", &mybuffer[3], mybuffer[3]);
-		// printf("addr[4]: %p : %x\n", &mybuffer[4], mybuffer[4]);
-		// printf("addr[5]: %p : %x\n", &mybuffer[5], mybuffer[5]);
-		// printf("addr[6]: %p : %x\n", &mybuffer[6], mybuffer[6]);
-		// printf("addr[7]: %p : %x\n", &mybuffer[7], mybuffer[7]);
-		// printf("addr[8]: %p : %x\n", &mybuffer[8], mybuffer[8]);
-		// printf("addr[9]: %p : %x\n", &mybuffer[9], mybuffer[9]);
-		// printf("addr[10]: %p : %x\n", &mybuffer[10], mybuffer[10]);
-		// printf("addr[11]: %p : %x\n", &mybuffer[11], mybuffer[11]);
-		// printf("addr[12]: %p : %x\n", &mybuffer[12], mybuffer[12]);
-		// printf("addr[13]: %p : %x\n", &mybuffer[13], mybuffer[13]);
-		// printBits(*(unsigned int *)mybuffer);
-		// printf("\n");
-		// return ;
 		u_int64_t read_buffer = read(fd, &buffer, SHA256_BLOCK_LEN);
 		total_read += (read_buffer * 8);		// Convert the numbers of octets read and add to total numbers of bits read
 												// If buffer read is > 56oct, we can't add padding (8oct) but make new bloc message and hash him.
-												// Use result of sha256_hash to init next ABCD variables
 		if (read_buffer != SHA256_BLOCK_LEN && !bit_padding_ok)
 		{
 			((u_int8_t *)&buffer.w)[read_buffer] = (char)128;  // add the last 1 bit at end of buffer (128 = 10000000)
 			bit_padding_ok = true;
 		}
+		convert_big_endian_msg(buffer, &buffer);
 		if (read_buffer == SHA256_BLOCK_LEN)
 			sha256_hash(&buffer, mdbuffer);
 		else if (read_buffer > SHA256_BLOCK_LEN_WITHOUT_PADDING)
-		{
-			convert_big_endian_msg(buffer, &buffer);
 			sha256_hash(&buffer, mdbuffer);
-		}
 		else
 		{
-			// printf("\n*******************\n");
-			// printBits(buffer.w[0]);
-			// printf("\n*******************\n");
-			// total_read = 4831838208;
-			convert_big_endian_msg(buffer, &buffer);
-			// printBits(buffer.w[0]);
 			*(uint32_t *)&(buffer.w[14]) = ((u_int32_t *)&total_read)[1]; // add padding
 			*(uint32_t *)&(buffer.w[15]) = ((u_int32_t *)&total_read)[0]; // add padding
 			sha256_hash(&buffer, mdbuffer);
@@ -150,10 +113,10 @@ static void init_mdbuffers(t_sha_buf *vars)
 	};
 }
 
-static void print_hash(u_int8_t *res)
+static void print_hash(u_int32_t *res)
 {
-	for(int i = 0; i < 32; ++i){
-		printf("%02x", res[i]);
+	for(int i = 0; i < 8; ++i){
+		printf("%08x", res[i]);
 	}
 }
 void hash_sha256(t_sha256_conf *conf)
