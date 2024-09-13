0154f57355de34a0a58cd29be217ce25271c5ca8
snaker <loic.masson67@hotmail.fr>
2024-09-13 10:26:42 +0100

    dev: refactor main

--- srcs/main.c
+++ srcs/main.c
@@ -1,51 +1,34 @@
 #include "../include/ft_ssl.h"
 
-static void ssl(t_conf *conf)
-{
-	switch(conf->ssl_conf.cmd)
-	{
-		case MD5:
-		{
-			md5((t_digest_conf *)conf);	
-			break ;
-		}
-		case SHA256:
-		{
-			hash_sha256((t_digest_conf *)conf);
-			break ;
-		}
-		default:
-		{
-		}
-	}
-}
-
-static void init_ssl_conf(t_conf *conf, t_args *args)
+static void ssl(t_conf *conf, t_args *args)
 {
 	switch(args->ssl_conf.cmd)
 	{
 		case MD5:
 		{
-			digest_init_conf((t_digest_args *)args, (t_digest_conf *)conf);	
+			digest_init_conf((t_digest_args *)args, (t_digest_conf *)conf);
+			md5((t_digest_conf *)conf);	
 			break ;
 		}
 		case SHA256:
 		{
 			digest_init_conf((t_digest_args *)args, (t_digest_conf *)conf);
+			hash_sha256((t_digest_conf *)conf);
 			break ;
 		}
 		default:
 		{
+			fprintf(stderr, "ft_ssl: bad command\n");
+			exit(1);
 		}
 	}
-
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 	t_args ssl_args;
 	t_conf ssl_conf;
 
 	ssl_parser(argc, argv, &ssl_args);
-	init_ssl_conf(&ssl_conf, &ssl_args);
-	ssl(&ssl_conf);
+	ssl(&ssl_conf, &ssl_args);
 }
